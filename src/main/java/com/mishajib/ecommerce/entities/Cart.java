e1f2a92d5cc32833fee0162f9f05f49d36574608
mi-shajib <mishajib222@gmail.com>
2025-08-30 23:55:16 +0600

    Added cart and cartItem entity or models

---
+++ src/main/java/com/mishajib/ecommerce/entities/Cart.java
@@ -0,0 +1,28 @@
+package com.mishajib.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+}
